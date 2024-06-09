6ddb8c17138830dd2d9d7813951ffb70ce4b556c
Yurii-ua74 <postffild@gmail.com>
2024-06-09 11:20:32 +0300

    Initial commit

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    // Посилання на компонент того ж ГО, на якому скріпт
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("BirdScript Start");
+        // одержуємо посилання
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.AddForce(new Vector2(0, 500));
+        }
+    }
+}
